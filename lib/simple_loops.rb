fe16e63d4723b603617a5f85e5335376218aaa88
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-05-27 15:55:41 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,3 +6,11 @@ def loop_message_five_times (message)
    count +=1
 end
 end
+
+def loop_message_n_times (message)
+  count = 0 
+  while count < number do 
+    puts message
+    count += 1 
+  end
+end 
